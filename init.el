ccf2eeddb7bea441e9330721e1db12247a9ee2ab
Lukasz Dziurzynski <lukasz.dziurzynski@airbnb.com>
2013-05-14 10:34:42 -0700

    added yasnippet and updated init.el for various things like R and hive

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings ess)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings ess yasnippet)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -16,3 +16,20 @@
     (package-install p)))
 
 (require 'ess-site)
+
+(auto-fill-mode -1)
+
+;; (setq x-select-enable-clipboard t)
+;; (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas/global-mode 1)
+
+;; Develop and keep personal snippets under ~/.emacs.d/plugins/mysnippets
+(setq yas/root-directory "~/.emacs.d/plugins/mysnippets")
+
+;; Load the snippets
+(yas/load-directory yas/root-directory)
+
+(setq auto-mode-alist (cons '("\\.hql$" . sql-mode) auto-mode-alist))
